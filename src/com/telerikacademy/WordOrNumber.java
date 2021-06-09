1ecd7010183db07e744308a45739961d55ac7c75
AntonBozhinov <anton.bozhinov@gmail.com>
2021-06-09 22:19:59 +0300

    clean up a bit

--- src/com/telerikacademy/WordOrNumber.java
+++ src/com/telerikacademy/WordOrNumber.java
@@ -4,52 +4,51 @@ import java.util.Scanner;
 
 public class WordOrNumber {
 
-
     public static void main(String[] args) {
-        StringBuilder words = new StringBuilder();
         int sum = 0;
-
+        StringBuilder words = new StringBuilder();
         Scanner scanner = new Scanner(System.in);
-
         // get the expected number of lines
         int lines = scanner.nextInt();
-
         for (int i = 0; i < lines; i++) {
             String input = null;
             // get the next input
             if (scanner.hasNext()) {
                 input = scanner.next();
             }
-
             // null check just for safety
             if (input != null) {
                 try {
                     // if can be parsed to integer add to sum and print the words so far
                     int number = Integer.parseInt(input);
                     sum += number;
-                    if (words.length() != 0) {
-                        System.out.println(words.deleteCharAt(words.length()-1));
-                        words = new StringBuilder();
-                    }
+                    maybePrintWords(words);
+                    words = new StringBuilder();
                 } catch (NumberFormatException e) {
                     // if not an int then append to words and print the sum so far
                     words.append(input).append("-");
-                    if (sum != 0) {
-                        System.out.println(sum);
-                        sum = 0;
-                    }
+                    maybePrintSum(sum);
+                    sum = 0;
                 }
 
             }
         }
         // print the final result
+        maybePrintWords(words);
+        maybePrintSum(sum);
+        scanner.close();
+    }
+
+    private static void maybePrintWords(StringBuilder words) {
         if (words.length() != 0) {
-            System.out.println(words.deleteCharAt(words.length()-1));
+            System.out.println(words.deleteCharAt(words.length() - 1));
         }
+    }
+
+    private static void maybePrintSum(int sum) {
         if (sum != 0) {
             System.out.println(sum);
         }
-        scanner.close();
     }
 }
 
